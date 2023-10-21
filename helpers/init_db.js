b724b1f8a6ebd1ee3ce172f0800523a7cd6f678d
Shan <shanchathuranga@gmail.com>
2023-10-22 00:17:33 +0530

    reverted to online database

--- helpers/init_db.js
+++ helpers/init_db.js
@@ -2,16 +2,16 @@ const Pool = require('pg').Pool
 
 const connectionString = process.env.CONN_STRING
 
-// const pool = new Pool({
-//     connectionString
-// })
-
 const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,    
-    password: process.env.POSTGRES_USERPW,
-    database: process.env.POSTGRES_DBNAME,    
+    connectionString
 })
+
+// const pool = new Pool({
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     user: process.env.POSTGRES_USER,    
+//     password: process.env.POSTGRES_USERPW,
+//     database: process.env.POSTGRES_DBNAME,    
+// })
 
 module.exports = pool
